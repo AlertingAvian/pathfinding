574d5ef5e95b977d274ddee2ccb2862526464e02
Patrick <boyscoutpatrick813@gmail.com>
2022-02-12 14:56:53 -0600

    test drawing on canvas

--- pathfinding/manage_map/display_map.py
+++ pathfinding/manage_map/display_map.py
@@ -28,7 +28,7 @@ class Window(object):
     Class for managing the window.
     """
 
-    def __init__(self, map_width: int, map_height: int, title: str, cell_size: int = 10, padding: int = 40):
+    def __init__(self, map_width: int, map_height: int, title: str, cell_size: int = 40, padding: int = 40):
         self.map_width = map_width
         self.map_height = map_height
         self.width = map_width * 10 + (padding * 2)
@@ -41,6 +41,8 @@ class Window(object):
                        [sg.Button('Start', key='start'), sg.Button('Stop', key='stop', disabled=True), sg.Button('Reset', key='reset'), sg.Button('Quit', key='quit', button_color=('white', 'red'))]]
         
         self.window = sg.Window(self.title, layout=self.layout)
+        self.canvas = self.window['canvas']
+        self.canvas.TKCanvas.itemconfig(self.canvas.TKCanvas.create_polygon(0, 0, cell_size, cell_size, bg='white'))
         self.window.finalize()
 
 test_window = Window(10, 10, 'Test Window')
