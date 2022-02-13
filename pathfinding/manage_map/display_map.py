082e44966f1bff0c82726b19b291fc42a6adec29
Patrick <boyscoutpatrick813@gmail.com>
2022-02-12 21:31:50 -0600

    Display in progress, moving everything to a class

--- pathfinding/manage_map/display_map.py
+++ pathfinding/manage_map/display_map.py
@@ -4,9 +4,10 @@
 
 from make_map import MapPoint
 
-from typing import List
+from typing import List, Tuple
 from os import system, name
 from termcolor import colored
+from dataclasses import dataclass
 
 
 class DisplayPoint(MapPoint):
@@ -25,6 +26,91 @@ class DisplayPoint(MapPoint):
 
     def __reper__(self):
         return super.__repr__(self)
+
+# TODO: finish move
+# TODO: provide way to inform search if exit is found
+
+@dataclass
+class CellInfo:
+    """
+    Class used to store information about a cell.
+    up, down, left, right: bool - True if the above cell is obstructed, None - if there is no cell above
+    """
+    up: bool | None
+    down: bool | None
+    left: bool | None
+    right: bool | None
+
+
+class MapDisplay(object):
+    current_cell: Tuple[int, int] = None
+    last_cell: Tuple[int, int] = None
+
+    def __init__(self, array: List[List[MapPoint, ]]):
+        self.map = self.__convert_mappoints_to_displaypoints(array)
+    
+    def __convert_mappoints_to_displaypoints(self, array: List[List[MapPoint, ]]) -> List[List[DisplayPoint, ]]:
+        """
+        Converts a 2D array of MapPoints to a 2D array of DisplayPoints.
+        """
+        x_len = len(array)
+        y_len = len(array[0])
+        display_array = [[DisplayPoint(j, i)
+                        for i in range(y_len)] for j in range(x_len)]
+        for x, row in enumerate(array):
+            for y, point in enumerate(row):
+                match point:
+                    case point if point.start:
+                        display_array[x][y].start = True
+                    case point if point.end:
+                        display_array[x][y].end = True
+                    case point if point.obstruction:
+                        display_array[x][y].obstruction = True
+                    case _:
+                        pass
+        return display_array
+
+    def move(self, direction: str, array: List[List[DisplayPoint]]):
+        """
+        Moves the active cell to the next cell in the given direction.
+        """
+        if not isinstance(array[0][0], DisplayPoint):
+            raise TypeError('The array must be a 2D array of DisplayPoints.')
+        if self.current_cell is not None:
+            x, y = self.current_cell
+            array[x][y].active = False
+            self.last_cell = self.current_cell
+        if direction == "up":
+            self.current_cell = (self.current_cell[0] - 1, self.current_cell[1])
+        elif direction == "down":
+            self.current_cell = (self.current_cell[0] + 1, self.current_cell[1])
+        elif direction == "left":
+            self.current_cell = (self.current_cell[0], self.current_cell[1] - 1)
+        elif direction == "right":
+            self.current_cell = (self.current_cell[0], self.current_cell[1] + 1)
+        else:
+            raise ValueError(f'Invalid move -> {direction}\nMust be one of: "up", "down", "left", "right"')
+        # NOT FINISHED
+
+    def __get_cell_information(self, array: List[List[DisplayPoint]], coords: Tuple[int, int]) -> CellInfo:
+        x, y = coords
+        if x == 0:
+            up = None
+        else:
+            up = array[x-1][y].obstruction
+        if x == len(array) - 1:
+            down = None
+        else:
+            down = array[x+1][y].obstruction
+        if y == 0:
+            left = None
+        else:
+            left = array[x][y-1].obstruction
+        if y == len(array[0]) - 1:
+            right = None
+        else:
+            right = array[x][y+1].obstruction
+        return CellInfo(up, down, left, right)
 
 
 def __display(array: List[DisplayPoint]) -> str:
@@ -47,9 +133,11 @@ def __display(array: List[DisplayPoint]) -> str:
                 case _:
                     match point:
                         case point if point.active:
-                            output_string += colored(' ', active_color, attrs=['reverse'])
+                            output_string += colored(' ',
+                                                     active_color, attrs=['reverse'])
                         case point if point.searched:
-                            output_string += colored(' ', searched_color, attrs=['reverse'])
+                            output_string += colored(' ',
+                                                     searched_color, attrs=['reverse'])
                         case _:
                             output_string += ' '
         output_string += colored(' ', 'white', attrs=['reverse'])
@@ -58,31 +146,12 @@ def __display(array: List[DisplayPoint]) -> str:
     return output_string
 
 
-def convert_mappoints_to_displaypoints(array: List[MapPoint]) -> List[DisplayPoint]:
-    """
-    Converts a 2D array of MapPoints to a 2D array of DisplayPoints.
-    """
-    x_len = len(array)
-    y_len = len(array[0])
-    display_array = [[DisplayPoint(j, i) for i in range(y_len)] for j in range(x_len)]
-    for x, row in enumerate(array):
-        for y, point in enumerate(row):
-            match point:
-                case point if point.start:
-                    display_array[x][y].start = True
-                case point if point.end:
-                    display_array[x][y].end = True
-                case point if point.obstruction:
-                    display_array[x][y].obstruction = True
-                case _:
-                    pass
-    return display_array
-
-
-def update_display(array: List[DisplayPoint]) -> None:
+def update_display(array: List[List[DisplayPoint, ]]) -> None:
     """
     Updates the display of the map.
     """
+    if not isinstance(array[0][0], DisplayPoint):
+        raise TypeError('The array must be a 2D array of DisplayPoints.')
     output_string = __display(array)
     clear_terminal()
     print(output_string)
@@ -93,3 +162,26 @@ def clear_terminal() -> None:
         _ = system('cls')
     else:
         _ = system('clear')
+
+
+def get_cell_information(array: List[List[DisplayPoint]], coords: Tuple[int, int]) -> CellInfo:
+    if not isinstance(array[0][0], DisplayPoint):
+        raise TypeError('The array must be a 2D array of DisplayPoints.')
+    x, y = coords
+    if x == 0:
+        up = None
+    else:
+        up = array[x-1][y].obstruction
+    if x == len(array) - 1:
+        down = None
+    else:
+        down = array[x+1][y].obstruction
+    if y == 0:
+        left = None
+    else:
+        left = array[x][y-1].obstruction
+    if y == len(array[0]) - 1:
+        right = None
+    else:
+        right = array[x][y+1].obstruction
+    return CellInfo(up, down, left, right)
